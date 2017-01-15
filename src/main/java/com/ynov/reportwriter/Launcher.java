b4ae1741f119fbd5db62e673aa94014f1bc7215c
rbeneteau <r.beneteau@outlook.fr>
2017-01-16 00:36:04 +0100

    Edit report printing to work with any kind of Printable element

--- src/main/java/com/ynov/reportwriter/Launcher.java
+++ src/main/java/com/ynov/reportwriter/Launcher.java
@@ -8,30 +8,34 @@ public class Launcher {
         Employee 		harry 	= new Employee("Harry", "Covert", 1400, 1);
         Student 		justin 	= new Student("Justin", "Calva", "EPITECH");
 
+        Document ultraTopSecret = new Document();
+        ultraTopSecret.setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu elit magna. Pellentesque at turpis non arcu gravida aliquet pellentesque at massa. Donec nunc elit, tincidunt non egestas a, placerat ac urna. Sed tempus odio nec sodales commodo. Maecenas sit amet imperdiet orci, ut gravida purus.");
+
         // Chacune des classes precedentes peut être considérée comme
-        // un salarié (héritage, liaison de type IS-A)
-        // et donc rentrer dans un tableau de salarié
-        Worker[] workers = new Worker[] {
+        // un élément imprimable (implémentation d'interface+héritage, liaison de type IS-A)
+        // et donc rentrer dans un tableau de Printable
+        Printable[] printableElements = new Printable[] {
                 alain, alex, anna,
-                harry, justin
+                harry, justin,
+                ultraTopSecret
         };
 
         // Affichage des données
-        printReport(workers);
+        printReport(printableElements);
     }
 
     /**
-     * Permet l'impression d'un rapport sur les salaries.
-     * @param workers
-     * 			Tableau de salaries dont on veut afficher les donnees.
+     * Permet l'impression d'un rapport.
+     * @param elementsToPrint
+     * 			Tableau d'éléments imprimables dont on veut afficher les donnees.
      */
-    public static void printReport(Worker[] workers) {
+    public static void printReport(Printable[] elementsToPrint) {
         System.out.println("Great Report");
         System.out.println("-------------------\n\n");
 
-        for(int i = 0; i < workers.length; i++) {
+        for(int i = 0; i < elementsToPrint.length; i++) {
             // Affichage des infos sur le salarie d'index I
-            workers[i].print();
+            elementsToPrint[i].print();
             System.out.println(""); // Saute une ligne
         }
     }
