18b4c5eb06d5fe4c16ccfccef909ab44d55823b5
rbeneteau <r.beneteau@outlook.fr>
2017-01-15 23:25:06 +0100

    Create instances for basic instances

--- src/main/java/com/ynov/reportwriter/Launcher.java
+++ src/main/java/com/ynov/reportwriter/Launcher.java
@@ -2,6 +2,12 @@ package com.ynov.reportwriter;
 
 public class Launcher {
     public static void main(String[] args) {
-        // Entry point.
+        Employee 		alain 	= new Employee("Alain", "Térieur", 1000, 14);
+        Student         alex 	= new Student("Alex", "Ception", "YNOV");
+        SubContractor   anna 	= new SubContractor("Anna", "Tomie", 150);
+
+        alain.print();
+        alex.print();
+        anna.print();
     }
 }
