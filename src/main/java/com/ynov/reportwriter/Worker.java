e0f1a9219e8811df5b98f97afb1a7ec9303606a9
rbeneteau <r.beneteau@outlook.fr>
2017-01-15 23:56:23 +0100

    Add Worker super type

---
+++ src/main/java/com/ynov/reportwriter/Worker.java
@@ -0,0 +1,67 @@
+package com.ynov.reportwriter;
+
+/**
+ * Classe desigant un salarie. <br />
+ * La classe est abstraite: on ne peut pas l'instancier (pas de "Worker worker = new Worker()").
+ */
+public abstract class Worker {
+    /*
+	 * ---------------------------------
+	 *		Propriétés
+	 * ---------------------------------
+	 */
+    /** Prénom. */
+    private String firstName;
+    /** Nom. */
+    private String lastName;
+
+    /*
+	 * ---------------------------------
+	 *		Constructeurs
+	 * ---------------------------------
+	 */
+
+    /**
+     * Constructeur par défault.
+     */
+    public Worker() {
+
+    }
+
+    /**
+     * Constructeur surcharge.
+     * @param firstName
+     * 			Prenom du salarie.
+     * @param lastName
+     * 			Nom de famille du salarie.
+     */
+    public Worker(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /*
+	 * ---------------------------------
+	 *		Méthodes
+	 * ---------------------------------
+	 */
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName.toUpperCase();
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Affiche la description d'un salarie (prenom, nom).
+     */
+    public void print() {
+        System.out.println(firstName+" "+getLastName());
+    }
+}
