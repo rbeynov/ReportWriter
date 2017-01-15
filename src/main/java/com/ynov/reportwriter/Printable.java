d8ab12b7a0ff0a4959a32268d79b5d82eb78fa4d
rbeneteau <r.beneteau@outlook.fr>
2017-01-16 00:31:50 +0100

    Create Printable interface

---
+++ src/main/java/com/ynov/reportwriter/Printable.java
@@ -0,0 +1,7 @@
+package com.ynov.reportwriter;
+
+public interface Printable {
+    // No body part for the method because it must be specificly implemented by every class that implements the interface
+    // public visibility access modifier is implicit, as well as abstract keyword !
+    void print();
+}
